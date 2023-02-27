c585e3b9a3db9994abd065f8bb2f5ccfb7bc2a7b
anandvipul <anandvipul01@gmail.com>
2023-02-27 17:32:33 +0530

    onClick Event Handler

--- block-BRaaas/code/handle-event/src/App.js
+++ block-BRaaas/code/handle-event/src/App.js
@@ -5,6 +5,8 @@ import Component1 from "./Components/Component1/Component1";
 import Component2 from "./Components/Component2/Component2";
 import "./Components/component.css";
 import Component3 from "./Components/Component3/Component3";
+import Component4 from "./Components/Component4/Component4";
+import Component5 from "./Components/Component5/Component5";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Component1></Component1>
         <Component2></Component2>
         <Component3></Component3>
+        <Component4></Component4>
+        <Component5></Component5>
       </div>
     </div>
   );
